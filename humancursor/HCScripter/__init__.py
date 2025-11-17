6cafbbe35986ed8a862845e20a196cd4de713d9b
Axel <sacca.axel@gmail.com>
2025-11-17 20:48:16 -0300

    Initialize __init__.py with module docstring and import statements

---
+++ humancursor/HCScripter/__init__.py
@@ -0,0 +1,6 @@
+"""HumanCursor Scripter - Record and playback mouse automation scripts."""
+
+from humancursor.HCScripter.gui import HCSWindow
+from humancursor.HCScripter.all_in_one_gui import AllInOneHCS, launch_all_in_one
+
+__all__ = ['HCSWindow', 'AllInOneHCS', 'launch_all_in_one']
